b23731bd8dc8cc743a5cb927d5505c3a37e71949
celloAlbatross <buddhinai.k@ku.th>
2018-01-11 23:33:21 +0700

    Test coin rotate

---
+++ Tempium/Assets/Scripts/CoinPos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using UnityEngine;
+
+public class CoinPos : MonoBehaviour {
+	public Path path;
+	public GameNode node;
+	public GameNode startNode;
+	// Use this for initialization
+
+	private int currentDirection = GameNode.NORTH;
+	void Start () {
+		node = startNode;
+
+		transform.position = node.transform.position;
+		transform.rotation = Quaternion.LookRotation(node.adjacencyNode[currentDirection].GetDirection(0));
+//		rotation = transform.rotation;
+		path = node.adjacencyNode[currentDirection];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tempium/Assets/Scripts/Test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	public Path path;
+	public GameNode node;
+	public GameNode startNode;
+	// Use this for initialization
+	private Quaternion rotation;
+	private Quaternion destRotation;
+	private float n = 0.5f;
+	private Vector3 temp;
+	private Quaternion temp1;
+	private Quaternion startTemp;
+	private enum stateRotate {left,right};
+	private stateRotate myState;
+	private int currentDirection = GameNode.NORTH;
+	void Start () {
+		myState = stateRotate.right;
+		node = startNode;
+		transform.position = node.transform.position;
+
+		temp = transform.position;
+		temp.y += 1;
+		
+		transform.position = temp; 
+		startTemp = transform.rotation;
+
+//		transform.rotation = Quaternion.LookRotation(node.adjacencyNode[currentDirection].GetDirection(0));
+//		rotation = transform.rotation;
+		path = node.adjacencyNode[currentDirection];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myState == stateRotate.right) {rotateRight ();}
+		else if(myState == stateRotate.left) {rotateLeft ();}
+	}
+
+	void rotateRight () {
+		temp1 = transform.rotation;
+		print (transform.rotation);
+		temp1.y += 0.03f;
+		transform.rotation = temp1;
+		if (transform.rotation.y > 0.990f) {
+			myState = stateRotate.left;
+		}
+	}
+
+	void rotateLeft () {
+		temp1 = transform.rotation;
+		print (transform.rotation);
+		temp1.y -= 0.03f;
+		transform.rotation = temp1;
+		if (transform.rotation.y < 0.011f) {
+			myState = stateRotate.right;
+		}
+	}
+}
